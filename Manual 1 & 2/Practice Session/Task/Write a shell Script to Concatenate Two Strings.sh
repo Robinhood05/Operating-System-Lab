f0a2e51ddd3e5736b9eb811b32db6a3ceca381cf
Robin <99762946+Robinhood05@users.noreply.github.com>
2024-03-14 21:37:50 +0600

    Create Write a shell Script to Concatenate Two Strings.sh
    How you can merge two string

---
+++ Manual 1 & 2/Practice Session/Task/Write a shell Script to Concatenate Two Strings.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Prompt the user to enter the first string
+read -p "Enter the first string: " first_string
+
+# Prompt the user to enter the second string
+read -p "Enter the second string: " second_string
+
+# Concatenate the two strings
+concatenated_string="${first_string}${second_string}"
+
+# Print the concatenated string
+echo "Concatenated string: $concatenated_string"
