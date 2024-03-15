ec0e01f5177a615726fa46f0f0c61756f9eae4de
Robin <99762946+Robinhood05@users.noreply.github.com>
2024-03-15 12:28:15 +0600

    Update and rename Some Command to Some Command.sh

---
+++ Lab Manual 1 & 2/Practice Session/Some Command.sh
@@ -0,0 +1,36 @@
+
+# Create a directory
+mkdir my_directory
+
+# Change directory to the created directory
+cd my_directory
+
+# Create a file
+touch my_file.txt
+
+# List files in the directory
+ls
+
+# Print current working directory
+pwd
+
+# Move the file to a new location
+mv my_file.txt ../
+
+# List files in the parent directory
+ls ..
+
+# Copy the file to a new location
+cp ../my_file.txt .
+
+# Remove the original file
+rm ../my_file.txt
+
+# Change directory to the parent directory
+cd ..
+
+# Remove the directory
+rmdir my_directory
+
+# Clear the terminal screen
+clear
